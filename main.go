6ae18265a259d7ca0eb03f9c39da046c23e5f9ea
Dima Pohiba <madi.nickname@gmail.com>
2023-04-17 23:25:48 +0300

    change provider address

--- main.go
+++ main.go
@@ -17,6 +17,6 @@ func main() {
 		// provider address is used in these tutorials in conjunction with a
 		// specific Terraform CLI configuration for manual development testing
 		// of this provider.
-		Address: "hashicorp.com/gofsd/terraform-provider-fsd",
+		Address: "hashicorp.com/gofsd/fsd",
 	})
 }
